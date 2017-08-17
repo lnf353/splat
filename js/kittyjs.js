d6804e41cade9ed436720b908756b748e039b082
EP <masogin@gmail.comD>
2017-08-17 23:04:03 +0300

    first slider absolutly DONE

--- js/kittyjs.js
+++ js/kittyjs.js
@@ -9,7 +9,11 @@
 	barSlideWidth = Math.floor(bar.offsetWidth /iterationCount),
 	fix=0,
 	lastPositionFix = bar.offsetWidth - barSlideWidth * iterationCount,
-	maxPosition = bar.offsetWidth - barSlideWidth - lastPositionFix;
+	maxPosition = bar.offsetWidth - barSlideWidth - lastPositionFix,
+	untilBarWidth = bar.getBoundingClientRect(),
+	animationIndex=0;
+        var currentAnimate = false;
+
     
     if(lastPositionFix % iterationCount != 0 ) {
 	fix = (lastPositionFix+1)/iterationCount;
@@ -18,23 +22,51 @@
     }
     barSlide.style.width = barSlideWidth + "px";
     
-    var kittyWidth = 220,// 150 + padding 
+    var kittyWidth = 240,// 150 + padding 
 	kittyCount = 4,
 	kPosition = 0,
 	kittyBoss = document.querySelector(".overflow"),
 	kittyUl = kittyBoss.querySelector(".description-slider"),
 	kittyLi = kittyBoss.querySelectorAll(".description-slider__slide");
+    
     function kittyLeft(){
 	kPosition = Math.min(kPosition + kittyWidth * kittyCount,0)
-	kittyUl.style.marginLeft = kPosition+ "px";
+	var leftAnimate = setInterval(function(){
+	    currentAnimate=true;
+	    animationIndex += 20;
+	    kittyUl.style.marginLeft = animationIndex + "px"
+	    if(animationIndex>=kPosition){
+		kittyUl.style.marginLeft = animationIndex + "px"
+		currentAnimate = false; 		
+		clearInterval(leftAnimate);
+	    } 
+	},5)
     }
+
     function kittyRight(){
+	
 	kPosition = Math.max(kPosition - kittyWidth * kittyCount, -kittyWidth * (kittyLi.length - kittyCount));
-	kittyUl.style.marginLeft = kPosition + "px"
+
+	var rightAnimate = setTimeout(function ra(){
+	    currentAnimate = true;
+	    animationIndex -= 20;
+	    kittyUl.style.marginLeft = animationIndex + "px"
+	    rightAnimate = setTimeout(ra,4)
+	    if(animationIndex <= kPosition){
+		currentAnimate = false; 		
+		clearInterval(rightAnimate);
+	    } 
+	},4)
+	
+	
+	
+	
+
     }
     
     var right = function(e) {
-	if(!(e.clientX >= bar.offsetWidth/2)) return;
+	if(currentAnimate) return; 
+	if( !(e.clientX >= (untilBarWidth.left +bar.offsetWidth/2)) ) return;
 	if(position >= maxPosition ) {
 	    return;
 	};
@@ -45,9 +77,10 @@
     }
 
     var left = function(e) {
-	if(!(e.clientX <= bar.offsetWidth/2)) return;
-	console.log(e.clientX);
+	if(currentAnimate) return; 
+	if(!(e.clientX <= (untilBarWidth.left + bar.offsetWidth/2))) return;
 	if(position <=0) return;
+	
 	cursorPosition -= 1;
 	position = cursorPosition * barSlideWidth - fix;
 	barSlide.style.marginLeft = position - fix + "px";
