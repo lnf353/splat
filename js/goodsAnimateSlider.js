c540d737a2933418c7a51b7b745a02f2a0feb493
E P <masogin@gmail.com>
2017-08-22 23:11:27 +0300

    moveable list of name hasn't been done

--- js/goodsAnimateSlider.js
+++ js/goodsAnimateSlider.js
@@ -1,20 +1,24 @@
 ;
 (function() {
-    
+
     var motherSlidesBlock =  document.querySelector("#motherSliderBlock");
+	var nameBlock = document.querySelector("#nameBlock");
     var rightArrow = document.querySelector("#rightArrow"),
 	leftArrow = document.querySelector("#leftArrow");
     
     var slideList = document.querySelectorAll("#slideContainer");
     var goodsNameList = document.querySelectorAll("#goodsNameText");
-    var nameBlock = document.querySelector("#nameBlock");
+	
+    var sizeBlock;
     var nameLiCoordinate;
     var oldTarget = nameBlock.children[0];
     var stepSize;
     var move=0;
     var selectedLi = nameBlock.children[0];
     var multiplySlideCount;
-    
+	var lastBlockCoordinate;
+	var firstBlockCoordinate; 
+	
     var hightLight = function(node) {
 	if(selectedLi) {
 	    selectedLi.classList.remove("goods-name--hight-light");
@@ -23,19 +27,39 @@
 	selectedLi.classList.add("goods-name--hight-light");
     };
 
+	var getsAllElementsSizes = function() {
+		lastBlockCoordinate = nameBlock.lastElementChild.getBoundingClientRect();
+		firstBlockCoordinate = nameBlock.firstElementChild.getBoundingClientRect();
+		sizeBlock = document.querySelector("#sizeControlBlock").getBoundingClientRect();
+		nameLiCoordinate = oldTarget.getBoundingClientRect();
+	}
+	getsAllElementsSizes();
+	
+	var checkThresholdOfRightLeft = function(e) {
+		console.log(firstBlockCoordinate.left);
+		if(lastBlockCoordinate.right >= sizeBlock.right && e.target.id == "rightArrow") {
+			nameBlock.style.marginLeft = -lastBlockCoordinate.right + sizeBlock.right + "px";
+		} else if (firstBlockCoordinate.left <= sizeBlock.left && e.target.id == "leftArrow") {
+			nameBlock.style.marginLeft = sizeBlock.left - firstBlockCoordinate.left  + "px";
+		}
+	};
+
     function shiftSlide(e) {
+		
 	for(var i = 0; i < e.target.parentNode.children.length; i++) {
 	    if(e.target.parentNode.children[i] == e.target){
 		multiplySlideCount = i;
 	    }
 	}
-	if( (nameBlock.lastElementChild.getBoundingClientRect().left + nameBlock.lastElementChild.getBoundingClientRect().width)  ) {}
-	nameLiCoordinate = oldTarget.getBoundingClientRect();
+		getsAllElementsSizes();
+		checkThresholdOfRightLeft(e);
+		
+
 	
 	if(e.target.tagName == "LI") { // and clientX < right side of e.target element
 	    oldTarget = e.target;
 	    stepSize=-960;
-	    if(e.clientX >= nameLiCoordinate.left+nameLiCoordinate.width) {
+	    if(e.clientX >= nameLiCoordinate.left + nameLiCoordinate.width) {
 		move = multiplySlideCount * stepSize; 
 	    } else if(e.clientX <= nameLiCoordinate.left) {
 		move = multiplySlideCount * stepSize; 
@@ -48,6 +72,7 @@
 	    oldTarget = oldTarget.nextElementSibling; 
 	    stepSize = -960;
 	    move = Math.max(move + stepSize, slideList.length * stepSize);
+
 	    
 	} else if (e.target == leftArrow) {
 	    if(oldTarget == nameBlock.children[0]) {return;}
